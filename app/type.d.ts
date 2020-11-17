8256397e7ca861dbae4e9d73971ff65f52e8bebd
Seba Bromberg <seba@syntag.ma>
2020-11-17 14:59:56 -0300

    Cambios de usuario

--- app/type.d.ts
+++ app/type.d.ts
@@ -5,14 +5,26 @@ type Persona= {
     cuit: string
 }
 
+type Usuario= {
+    nombre: string
+    apellido: string
+    email: string
+    cuit: string
+    sub: string
+    rol: string
+}
+
 type Apoderado= {
     persona: Persona
     esAdministrador: Boolean
 }
 
 type TramiteAlta = {
+    id: string
     razonSocial: string
     personería: string
     tipoEmpresa: string
     apoderados: Array<Apoderado>
+    status: "BORRADOR"|"A VERIFICAR"|"VERIFICADO"
+    propietario: Usuario
 }
