17f3d1db36d025c468890f4fe745d8936b17818d
Leonardo Leenen <leonardoleenen@Leonardos-MacBook-Pro.local>
2020-12-13 16:08:53 -0300

    Adding Wrapping store

--- app/stories/bandejaConstructor.stories.tsx
+++ app/stories/bandejaConstructor.stories.tsx
@@ -6,11 +6,17 @@ import {BandejaConstructor, BandejaConstructorProps} from '../components/bandeja
 import '../public/style/main.css'
 import 'antd/dist/antd.css'
 
+import { Provider } from 'react-redux'
+import withReduxStore from '../redux/lib/with-redux-store'
+import createStore from '../redux/store'
+
 export default {
   title: 'Example/Bandeja Constructor',
   component: BandejaConstructor,
 } as Meta;
 
-const Template: Story<BandejaConstructorProps> = (args) => <BandejaConstructor {...args} />;
+const reduxStore = createStore()
+
+const Template: Story<BandejaConstructorProps> = (args) => <Provider store={reduxStore}><BandejaConstructor {...args} /></Provider>
 
 export const General = Template.bind({});
