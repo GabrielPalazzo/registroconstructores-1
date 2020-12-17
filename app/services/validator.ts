73fa3d2b312bfada70067f2952e4948f8b0c72be
Leonardo Leenen <leonardoleenen@Leonardos-MacBook-Pro.local>
2020-12-17 10:47:34 -0300

    BIGFIX Anidamiento

--- app/services/validator.ts
+++ app/services/validator.ts
@@ -26,47 +26,47 @@ class Validator implements ValidatorInterface {
           error:'La razon social es requerida'
         })
 
-        if(!this.tramite.tipoEmpresa)
-          toValidate.push({
-            attribute:'tipoEmpresa',
-            dataId:'',
-            error:'Debera seleccionar al menos un tipo de empresa'
-          })
+      if(!this.tramite.tipoEmpresa)
+        toValidate.push({
+          attribute:'tipoEmpresa',
+          dataId:'',
+          error:'Debera seleccionar al menos un tipo de empresa'
+        })
 
-        if(!this.tramite.cuit)
-          toValidate.push({
-            attribute:'cuit',
-            dataId:'',
-            error:'El CUIT es obligatorio'
-          })
-        
-        if(!this.tramite.nroMatriculaComerciante)
-          toValidate.push({
-            attribute:'nroMatriculaComerciante',
-            dataId:'',
-            error:'El Nro de matricula es Obligatorio'
-          })
-        
-        if(!this.tramite.fechaInscripcionMatriculaComerciante)
-          toValidate.push({
-            attribute:'fechaInscripcionMatriculaComerciante',
-            dataId:'',
-            error:'La fecha de inscripción de la Matrícula de Comerciante en rubro Construcción no puede ser anterior al alta en AFIP'
-          })
+      if(!this.tramite.cuit)
+        toValidate.push({
+          attribute:'cuit',
+          dataId:'',
+          error:'El CUIT es obligatorio'
+        })
+      
+      if(!this.tramite.nroMatriculaComerciante)
+        toValidate.push({
+          attribute:'nroMatriculaComerciante',
+          dataId:'',
+          error:'El Nro de matricula es Obligatorio'
+        })
+      
+      if(!this.tramite.fechaInscripcionMatriculaComerciante)
+        toValidate.push({
+          attribute:'fechaInscripcionMatriculaComerciante',
+          dataId:'',
+          error:'La fecha de inscripción de la Matrícula de Comerciante en rubro Construcción no puede ser anterior al alta en AFIP'
+        })
 
-        if(!this.tramite.domicilioReal)
-          toValidate.push({
-            attribute:'domicilioReal',
-            dataId:'',
-            error:'Indique el domicilio real'
-          })
-        
-        if(!this.tramite.domicilioLegal)
-          toValidate.push({
-            attribute:'domicilioLegal',
-            dataId:'',
-            error:'Indique el domicilio legal '
-          })
+      if(!this.tramite.domicilioReal)
+        toValidate.push({
+          attribute:'domicilioReal',
+          dataId:'',
+          error:'Indique el domicilio real'
+        })
+      
+      if(!this.tramite.domicilioLegal)
+        toValidate.push({
+          attribute:'domicilioLegal',
+          dataId:'',
+          error:'Indique el domicilio legal '
+        })
 
       return toValidate
     }
