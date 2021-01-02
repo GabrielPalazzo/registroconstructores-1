42191a2d6acc4ce286b82505a9acb977352230fd
Leonardo Leenen <leonardoleenen@Leonardos-MacBook-Pro.local>
2021-01-02 10:56:10 -0300

    Primera version de swagger

---
+++ app/pages/api/tramite/getTramitesParaVerificar.ts
@@ -0,0 +1,20 @@
+import nextConnect from 'next-connect';
+import middleware from '../../../middlewares/middleware'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req: any, res: NextApiResponse) => {
+  console.log(req.user.Role)
+  const tramites = await req.db
+    .collection('tramites')
+    .find({'creatorId.iat': req.user.iat}
+    )
+    .toArray();
+  res.send({ tramites });
+});
+
+
+export default handler
--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -16,7 +16,10 @@ export default () => {
           }} block>Constructor</Button>
         </div>
         <div className="pb-4 text-center  w-full">
-          <Button type="primary" block >Revisor Back Office</Button>
+        <Button type="primary" onClick={() => {
+            localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJSZWdpc3RybyBkZSBDb25zdHJ1Y3RvcmVzIiwiaWF0IjoyMjMzNDQ1NTY2NzcsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJzdWIiOiJsZW9uYXJkb2xlZW5lbkBnbWFpbC5jb20iLCJHaXZlbk5hbWUiOiJNYXJ0aW4gIiwiU3VybmFtZSI6IklnbGVzaWFzIiwiRW1haWwiOiJsZW9uYXJkb2xlZW5lbkBnbWFpbC5jb20iLCJSb2xlIjpbIkNPTlRST0xBRE9yIl19.lgzTyC-E-VwH7wqnAQAe2d8Oivd8fBQheA8NpHNUTTo')
+            router.push('/backoffice/bandeja')
+          }} block>Revisor Back Office</Button>
         </div >
         <div className="pb-4  text-center w-full">
           <Button type="primary" block>Supervisor Back Office</Button>
