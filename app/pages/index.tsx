86ed2b47590096232bb6c288a74cb041883f8c65
Maria noel Leenen <marianoel.leenen@agtech.com.ar>
2021-03-05 14:52:12 -0300

    version 1.21

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -124,7 +124,7 @@ export default () => {
     </div>
     <div className="pb-4 text-center  border-gray-200 border-t-2" >
 
-     <div className="text-sm mt-2"> version: 1.20 </div> 
+     <div className="text-sm mt-2"> version: 1.21 </div> 
 
     </div>
 
