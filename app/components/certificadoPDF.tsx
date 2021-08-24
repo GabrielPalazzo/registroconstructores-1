0ca8608cb7e5418ea309102b74cdad5f7ab791e2
Maria noel Leenen <leenen.maria@gmail.com>
2021-08-24 12:08:16 -0300

    porueba

--- app/components/certificadoPDF.tsx
+++ app/components/certificadoPDF.tsx
@@ -1427,22 +1427,23 @@ export default (props) => {
        <View style={styles.sectionContentTable} >
           <View style={styles.sectionContentTableColumnBorder}  >
             <Text style={{ ...styles.sectionEtiquetaTable, fontWeight: 'heavy' }}>Razon SocialComitente</Text>
-            {props.certificado.tramite.ddjjObras.filter(o => o.status && o.status === 'APROBADA'|| o.status && o.status === 'SUPERVIZADA' ).filter( (o:DDJJObra) => _.includes(['Preadjudicada','Adjudicada','Ejecucion'],o.datosObra[0].estado)).map((o: DDJJObra) => <Text style={{ margin: 5 }}  >{o.razonSocialComitente}</Text>)}
+            {props.certificado.tramite.ddjjObras.filter(o => o.status && o.status === 'APROBADA' ).filter( (o:DDJJObra) => _.includes(['Preadjudicada','Adjudicada','Ejecucion'],o.datosObra[0].estado)).map((o: DDJJObra) => <Text style={{ margin: 5 }}  >{o.razonSocialComitente}</Text>)}
 
           </View>  
-
+{/** 
         <View style={styles.sectionContentTableColumnBorder3}  >
 
             <Text style={styles.sectionEtiquetaTable}>Denominación</Text>
-            {props.certificado.tramite.ddjjObras.filter(o => o.status && o.status === 'APROBADA' || o.status && o.status === 'SUPERVIZADA' ).filter( (o:DDJJObra) => _.includes(['Preadjudicada','Adjudicada','Ejecucion'],o.datosObra[0].estado)).map((o: DDJJObra) => <Text style={{ margin: 5 }} >{o.denominacion}</Text>)}
+            {props.certificado.tramite.ddjjObras.filter(o => o.status && o.status === 'APROBADA'  ).filter( (o:DDJJObra) => _.includes(['Preadjudicada','Adjudicada','Ejecucion'],o.datosObra[0].estado)).map((o: DDJJObra) => <Text style={{ margin: 5 }} >{o.denominacion}</Text>)}
 
           </View>
-       {/*  
+   */}    
           <View style={styles.sectionContentTableColumnBorder2}  >
             <Text style={styles.sectionEtiquetaTable}>Fecha de Adjudicación</Text>
             {props.certificado.tramite.ddjjObras.filter(o => o.status && o.status === 'APROBADA' ).filter( (o:DDJJObra) => _.includes(['Preadjudicada','Adjudicada','Ejecucion'],o.datosObra[0].estado)).map((o: DDJJObra) => <Text style={{ margin: 5 }}  >{o.datosObra[0].fechaAdjudicacion}</Text>)}
 
           </View>
+       
           <View style={styles.sectionContentTableColumnBorder}  >
             <Text style={styles.sectionEtiquetaTable}>Monto Contractual</Text>
             {props.certificado.tramite.ddjjObras.filter(o => o.status && o.status === 'APROBADA').filter( (o:DDJJObra) => _.includes(['Preadjudicada','Adjudicada','Ejecucion'],o.datosObra[0].estado)).map((o: DDJJObra) => <Text style={{ margin: 5 }} >{numeral(o.montoInicial).format('$0,0.00')}</Text>)}
@@ -1453,7 +1454,7 @@ export default (props) => {
 
           </View>
 
- */}
+  
 
         </View> 
 
