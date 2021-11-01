f258d1242788e492cf4841b11b976330a5b70823
Maria noel Leenen <leenen.maria@gmail.com>
2021-11-01 13:38:59 -0300

    se visualiza admin legitimado

--- app/pages/informacion_basica.tsx
+++ app/pages/informacion_basica.tsx
@@ -390,7 +390,7 @@ export default (props) => {
 
 
   ]
-  columns = getUsuario().isConstructor() ? columns.slice(1, columns.length ) : [columns[0], columns[1], columns[2], columns[3],  columns[4], columns[5], columns[6], columns[7], columns[8]]
+  columns = getUsuario().isConstructor() ? columns.slice(1, columns.length ) : [columns[0], columns[1], columns[2], columns[3],  columns[4], columns[5], columns[6], columns[7], columns[8], columns[9]]
 
 
 
