5a2de64361b6a393edf32a8c32970cf4955ecf73
Maria Noel Leenen <leenen.maria@gmail.com>
2021-11-29 15:20:56 -0300

    se comenta la black List

--- app/middlewares/checkJwt.ts
+++ app/middlewares/checkJwt.ts
@@ -4,19 +4,22 @@ import _ from 'lodash'
 
 const BLACK_LIST = ['20367628376']
 
+
 export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
  const authHeader = req.headers['authorization'] ? req.headers['authorization']  : 'Bearer ' + req.query.token
  
  
  const token = authHeader && authHeader.split(' ')[1] 
 
+
+
  if (!token) return res.status(401).send('Acceso denegado. Debe proveer un token') // if there isn't any token
  jwt.verify(token, process.env.SESSION_SECRET as string, (err: any, user: any) => {
    if (err) return res.status(403).send('Acceso denegado. Su token no es valido');
    req.user = user
     
-   if (!_.isEmpty(BLACK_LIST.filter(t =>  jwt.decode(token).cuit === t)))
-    return res.status(403).send('Acceso denegado. Su token no se encuentra habilitado para realizar esta operación');
+  // if (!_.isEmpty(BLACK_LIST.filter(t =>  jwt.decode(token).cuit === t)))
+    //return res.status(403).send('Acceso denegado. Su token no se encuentra habilitado para realizar esta operación');
 
    next() 
  }) 
